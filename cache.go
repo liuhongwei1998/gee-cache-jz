3063ddd5d9d0871ede7e06707883c2588060f526
liuhongwei02 <liuhongwei02@bilibili.com>
2022-10-12 22:28:38 +0800

    dev geecache

---
+++ cache.go
@@ -0,0 +1,36 @@
+package gee_cache_jz
+
+import (
+	"gee-cache-jz/lru"
+	"sync"
+)
+
+type cache struct {
+	mu  sync.Mutex
+	lru *lru.Cache
+
+	// 初始化最大可用内存
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
---
+++ geecache_test.go
@@ -0,0 +1,18 @@
+package gee_cache_jz
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Errorf("callback failed")
+	}
+}
