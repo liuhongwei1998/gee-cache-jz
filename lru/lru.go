69e2f63480709b4dbb15cd22506d93a5fe0212cd
liuhongwei02 <liuhongwei02@bilibili.com>
2022-10-12 18:45:33 +0800

    lru model and test success

--- lru/lru.go
+++ lru/lru.go
@@ -8,7 +8,8 @@ type Cache struct {
 	ll       *list.List
 	cache    map[string]*list.Element
 
-	OnEvivted func(key string, value Value)
+	// 记录被移除时的回调函数
+	OnEvicted func(key string, value Value)
 }
 
 type entry struct {
@@ -16,6 +17,69 @@ type entry struct {
 	value Value
 }
 
+//为了通用性，我们允许值是实现了 Value 接口的任意类型，
+//该接口只包含了一个方法 Len() int，
+//用于返回值所占用的内存大小。
 type Value interface {
 	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// Get value
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+
+	// 默认为false
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+
+		// callback
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+
+	// 如果最大可用内存不为0  一直淘汰最老元素 只到已用内存小于最大要求
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+
+}
+
+// Len is the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
 }
---
+++ lru/lru_test.go
@@ -0,0 +1,43 @@
+package lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+
+func TestCache_Get(t *testing.T) {
+	lru := New(int64(0), nil)
+	lru.Add("jz1", String("jz1"))
+	if v, ok := lru.Get("jz1"); !ok || string(v.(String)) != "jz1" {
+		t.Fatal("cache hit jz1 = jz1 failed")
+	}
+	if _, ok := lru.Get("jz2"); ok {
+		t.Fatal("cache miss jz2 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+
+	lru := New(int64(10), callback)
+
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+
+	expect := []string{"key1", "k2"}
+
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	}
+}
