5426a0b5d5b1ac02dcf1e5e47d1342bfef0a448a
liuhongwei02 <liuhongwei02@bilibili.com>
2022-10-13 14:48:24 +0800

    dev consistenthash.go

---
+++ consistenthash/consistenthash.go
@@ -0,0 +1,59 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// hash函数
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int
+
+	keys    []int // sorted 哈希环
+	hashMap map[int]string
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+
+	return m
+}
+
+// 添加节点到哈希环
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+
+	// 二分搜索
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
