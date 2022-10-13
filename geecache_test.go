8963668d10acca5cfd598641fe69ebc0f2ffd59e
liuhongwei02 <liuhongwei02@bilibili.com>
2022-10-13 11:32:25 +0800

    geecache add unit test complete

--- geecache_test.go
+++ geecache_test.go
@@ -1,9 +1,58 @@
 package gee_cache_jz
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
+
+// 用map模拟数据库
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGet(t *testing.T) {
+
+	// 记录回源次数
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+
+			// 回源逻辑
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+
+					// 没有回源
+					loadCounts[key] = 0
+				}
+				loadCounts[key]++
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		},
+	))
+
+	for k, v := range db {
+		if view, err := gee.Get(k); err != nil || view.String() != v {
+			t.Fatalf("failed %s", k)
+		}
+
+		// load from callback function
+		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+
+		// cache hit
+	}
+
+	if view, err := gee.Get("adasdas"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
+	}
+}
 
 func TestGetter(t *testing.T) {
 	var f Getter = GetterFunc(func(key string) ([]byte, error) {
